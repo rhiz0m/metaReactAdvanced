4b29414b33e3b88dc2d7bd3cb68533c23be4dc2b
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-11-26 17:26:48 +0100

    Refactor - adding sort method on items for ascending order

--- src/module1/lab_listsInReact/DessertsList.js
+++ src/module1/lab_listsInReact/DessertsList.js
@@ -1,6 +1,7 @@
 function DessertsList(props) {
   const listItems = props.data
   .filter(dessert => dessert.calories < 500)
+  .sort((a, b) => a.calories - b.calories)
   .map(dessert => {
     const itemText = `${dessert.name} - ${dessert.calories}`
     return <li key={dessert.name}>{itemText}</li>
