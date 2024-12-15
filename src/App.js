24a702e2c4b6ac026d7753d470073be3e92d52e7
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-12-15 18:31:37 +0100

    Feat - Gift Card App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AppModule1Forms from "./module1/lab_forms/AppModule1Forms"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import FetchDataAsyncAwait from "./module2/FetchDataAsyncAwait"
 import GoalApp from "./module2/GoalApp"
+import GiftCard from "./module2/Lab_giftCard"
 
 
 const queryClient = new QueryClient()
@@ -11,6 +12,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
+        <GiftCard />
         <GoalApp />
         <FetchDataAsyncAwait />
         <AppModule1Forms />
