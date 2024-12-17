968eb7f5872de351d61f4c77185bd03633049a3a
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-12-17 18:11:48 +0100

    Completing the HOC and Render Props

--- src/module3/AppScrollerPosition.js
+++ src/module3/AppScrollerPosition.js
@@ -10,6 +10,10 @@ const MousePosition = ({ render }) => {
   useEffect(() => {
     const handleMousePositionChange = (e) => {
       // Use e.clientX and e.clientY to access the mouse position on the screen
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      })  
     }
 
     window.addEventListener("mousemove", handleMousePositionChange)
@@ -19,37 +23,37 @@ const MousePosition = ({ render }) => {
     }
   }, [])
 
-  // What should be returned here?
-  return null
+  return render({ mousePosition })
 }
 
-// This component should not receive any props
-const PanelMouseLogger = ({ mousePosition }) => {
+const PanelMouseLogger = () => {
   // The below if statement can be removed after the render props pattern is implemented
-  if (!mousePosition) {
-    return null
-  }
   return (
     <div className="BasicTracker">
       <p>Mouse position:</p>
-      <div className="Row">
-        <span>x: {mousePosition.x}</span>
-        <span>y: {mousePosition.y}</span>
-      </div>
+      <MousePosition
+        render={({ mousePosition }) => (
+          <div className="Row">
+            <span>x: {mousePosition.x}</span>
+            <span>y: {mousePosition.y}</span>
+          </div>
+        )}
+      />
     </div>
   )
 }
 
-// This component should not receive any props
-const PointMouseLogger = ({ mousePosition }) => {
+const PointMouseLogger = () => {
   // The below if statement can be removed after the render props pattern is implemented
-  if (!mousePosition) {
-    return null
-  }
+ 
   return (
-    <p>
-      ({mousePosition.x}, {mousePosition.y})
-    </p>
+    <div>
+      <MousePosition render={({ mousePosition }) => (
+      <p>
+        ({mousePosition.x}, {mousePosition.y})
+      </p>
+  )} />
+    </div>
   )
 }
 
